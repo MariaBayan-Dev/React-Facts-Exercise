69aa863a48173941ef632e1880c0ad70198069d3
SakuHeli0s <mariabayanescobar@gmail.com>
2026-07-15 18:30:51 +0200

    Primer commit React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Main from './components/Main'
+import NavBar from './components/Navbar'
+
+export default function App() {
+  return (
+    <>
+    <NavBar />
+    <Main />
+    </>
+  )
+}
+
+
