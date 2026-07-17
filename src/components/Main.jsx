67647cf6dbe41e45cb08eb9af40f03449273beb6
SakuHeli0s <mariabayanescobar@gmail.com>
2026-07-17 20:18:31 +0200

    CSS main avanzado.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ export default function Main(){
         <main>
             <section>
                 <h1>Fun facts about React</h1>
-                <ul>
+                <ul className="facts-list">
                     <li>Was first released in 2013</li>
                     <li>Was originally created by Jordan Walke</li>
                     <li>Has well over 200K stars on Github</li>
